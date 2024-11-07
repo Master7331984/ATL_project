b8baac03e0549b61cfc8868378f477735fe0bcae
Master7331984 <huseynli733@gmail.com>
2024-11-07 15:57:29 +0400

    first commit

---
+++ src/Test.java
@@ -0,0 +1,236 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+
+public class Test {
+    public static void main(String[] args) {
+
+//           Scanner scan = new Scanner(System.in);
+//               System.out.println("1-ci ededi daxil edin:");
+//           var a= scan.nextInt();
+//               System.out.println("2-ci ededi daxil edin:");
+//           var b= scan.nextInt();
+//
+//           var      c= a+b;
+//               System.out.println(c);
+//                    c=a-b;
+//               System.out.println(c);
+//                    c=a*b;
+//               System.out.println(c);
+//                    c=a/b;
+//               System.out.println(c);
+            
+//         Scanner scan = new Scanner(System.in);
+//        System.out.println("Adinizi daxil edin:");
+//        String name=scan.nextLine();
+//        System.out.println("Soyadinizi daxil edin:");
+//        String surname= scan.nextLine();
+//        System.out.println("Yaşınızı daxil edin:");
+//        int old= scan.nextInt();
+//        System.out.println(name +" "+ surname+" "+ old);
+
+        //Ata-oğul yaşlarının hesablanması
+//          Scanner scan = new Scanner(System.in);
+//          System.out.println("Atanın yaşını daxil edin:");
+//        int fath_old = scan.nextInt();
+//        int son_old = scan.nextInt();
+//          System.out.println("Oğulun yaşı:"+ son_old);
+//
+//          System.out.println("Atanın yaşı: "+ fath_old+ " "+ "Oğulun yaşı: "+ son_old);
+
+        //Duzbucaqlinin perimetri ve sahesinin hesablanmasi
+//        Scanner scan = new Scanner(System.in);
+//           System.out.println("Duzbucaqlinin uzunlugunu daxil edin:");
+//        int a= scan.nextInt();
+//            System.out.println("Duzbucaqlinin enini daxil edin:");
+//        int b= scan.nextInt();
+//        int P= 2*(a+b);
+//            System.out.println(P);
+//        int S= a*b;
+//            System.out.println(S);
+        //yashin aylarla hesablanmasi
+//        Scanner scan = new Scanner(System.in);
+//              System.out.println("Yasi daxil edin:");
+//        int a= scan.nextInt();
+//        int yas= a*12;
+//              System.out.println("Yaş"+ " "+ yas);
+
+        //Kvadratinin sahesinin ve perimetrinin hesablanmasi
+//        Scanner scan= new Scanner(System.in);
+//        System.out.println("Ededi daxil edin:");
+//        int a= scan.nextInt();
+//        int S= a*a;
+//        int P=4*a;
+//        System.out.println("S="+S);
+//        System.out.println("P="+P);
+
+//        BigDecimal myBigDecimal1 = BigDecimal.valueOf(1.3);
+//        BigDecimal myBigDecimal2 = BigDecimal.valueOf(1.5);
+//        System.out.println(myBigDecimal1.add(myBigDecimal2));
+
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("a daxil edin:");
+//        int a= scan.nextInt();
+//        System.out.println("b daxil edin");
+//        int b= scan.nextInt();
+//        int c= a/b;
+//        int d= a%b;
+//        System.out.println("Tam hisse:"+" "+c);
+//        System.out.println("Kesr hisse:"+" "+d);
+        //Ededin mertebelere bolunmesi
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Ededi daxil edin:");
+//        int a= scan.nextInt();
+//        int b=a/10;
+//        System.out.println("onluq mertebesi:"+" "+b);
+//        int c=a%10;
+//        System.out.println("teklik mertebesi:"+ " "+c);
+//        int d=b*c;
+//        System.out.println(d);
+        //Ededin mertebelere bolunmesi
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Ededi daxil edin:");
+//        int a=scan.nextInt();
+//        int b=a/100;
+//        System.out.println("Yuzlukler mertebesi:"+" "+ b);
+//        int c=(a/10)%10;
+//        System.out.println("Onluqlar mertebesi:"+" "+  c);
+//        int d=a%10;
+//        System.out.println("Teklikler mertebesi:"+" "+ d);
+//        int e=b+c+d;
+//        System.out.println("Ededlerin cemi:"+" " + e);
+//            e=b-c-d;
+//        System.out.println("Ededlerin ferqi:"+" "+ e);
+//            e=b*c*d;
+//        System.out.println("Ededlerin hasili:"+" "+e);
+        // Ededlerin muqayisesi
+//        Scanner scan = new Scanner(System.in);
+//        int a= scan.nextInt();
+//        int b= scan.nextInt();
+//        System.out.println("Ededleri daxil edin"+ " "+ (a>b));
+//        System.out.println("Ededleri daxil edin"+" "+ (a<b));
+//        System.out.println("Ededleri daxil edin"+ " "+ (a==b));
+//        System.out.println("Ededleri daxil edin"+" "+ (a!=b));
+
+        //Ededlerin tersden oxunmasi
+//        Scanner scan = new Scanner(System.in);
+//        int a =scan.nextInt();
+//        System.out.println("a-ni daxil edin");
+//        int b= a/100;
+//        int c= a/10%10;
+//        int d= a%10;
+//        int e= d*100+c*10+b;
+//        System.out.println(e);
+       // Minlikler mertebesinde
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        System.out.println("ededi daxil edin:");
+//        int b= a/1000;
+//        int c=(a/100%10);
+//        int d=(a/10)%10;
+//        int e=a%10;
+//        int f= ((b*c)-(d+e));
+//        System.out.println(f);
+        //1-ci ve 4-cu ededlerin yerini deyishdirmek
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        System.out.println("ededi daxil edin:");
+//        int b= a/1000;
+//        int c=(a/100%10);
+//        int d=(a/10)%10;
+//        int e=(a%10);
+//        int f =e*1000+c*100+d*10+b;
+//        System.out.println(f);
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a= scanner.nextInt();
+//        boolean b=a%2==0;
+//        System.out.println("ededi daxil edin"+b);
+
+//        Scanner scan = new Scanner(System.in);
+//        int a= scan.nextInt();
+//        int b = a/100;
+//        int c=a%10;
+//        boolean d=(b==c);
+//        System.out.println("eded polendromdur"+ " " +d);
+        
+        //Dordreqemli ededin cut olmasi
+//        Scanner scan = new Scanner(System.in);
+//        int a= scan.nextInt();
+//        System.out.println("ededi daxil edin");
+//        int b= a/1000;
+//        boolean x=(a%2==0);
+//        System.out.println(x);
+//        int c= a/100%10;
+//        boolean y=(a%2==0);
+//        System.out.println(y);
+//        int d= a/10%10;
+//        boolean z=(a%2==0);
+//        System.out.println(z);
+//        int e= a%10;
+//        boolean f=(e%2==0);
+//        System.out.println(f);
+        //Dordreqemli ededin simmetrik olmasi
+//        Scanner scan = new Scanner(System.in);
+//        int a= scan.nextInt();
+//        System.out.println("ededi daxil edin:");
+//        int b= a/1000;
+//        int c= a/100%10;
+//        int d= a/10%10;
+//        int e= a%10;
+//        boolean f=(b==e && c==d);
+//        System.out.println(f);
+
+        //Dordreqemli ededin artan sirada duzulmesi
+//        Scanner scan = new Scanner(System.in);
+//        int a= scan.nextInt();
+//        System.out.println("ededi daxil edin:");
+//        int b= a/1000;
+//        int c= a/100%10;
+//        int d= a/10%10;
+//        int e= a%10;
+//        boolean f=(b<c && c<d && d<e);
+//        System.out.println(f);
+
+        // 3 ve ya 4 bolunme
+        Scanner scan = new Scanner(System.in);
+
+
+
+        System.out.println();
+
+//        Scanner scan= new Scanner(System.in);
+//        var Username="admin";
+//        var enteredUsername
+//        if(admin=="akif") {
+//            System.out.println("Sisteme giris ugurludur");
+//        }
+//            else{
+//                System.out.println("Sisteme girish ugursuzdur");
+//            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
